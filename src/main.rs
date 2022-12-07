b5ed7824d958a52656d99fe324f92d96e96537db
Leopold Wigbratt <leopold.wigbratt@gmail.com>
2022-12-08 00:22:37 +0100

    Completed day 7

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 type SolverResult = Result<(String, String), Box<dyn Error>>;
 type Solver = fn() -> SolverResult;
 
-const DAYS: usize = 6;
-const SOLVERS: [Solver; DAYS] = [day1::solve, day2::solve, day3::solve, day4::solve, day5::solve, day6::solve];
+const DAYS: usize = 7;
+const SOLVERS: [Solver; DAYS] = [day1::solve, day2::solve, day3::solve, day4::solve, day5::solve, day6::solve, day7::solve];
 
 fn main() {
     for i in 0..DAYS {
